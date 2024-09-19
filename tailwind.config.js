a0772db9230a0a90497bcdde2cd6b06035397a1f
fdimopoulos <fdimopoulos@hotmail.com>
2024-09-19 21:06:39 +0300

    edit the white color and change the max-with of the description to 65ch

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    darkMode: "class",
-    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-    theme: {
-        extend: {
-            colors: {
-                blackish: "#121212",
-                whitish: "#f2f2f2",
-            },
-            boxShadow: {
-                shadow: "2px 2px 5px rgba(0, 0, 0, 0.2)",
-            },
-        },
+  darkMode: "class",
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        blackish: "#121212",
+        whitish: "#f5f5f5",
+      },
+      boxShadow: {
+        shadow: "2px 2px 5px rgba(0, 0, 0, 0.2)",
+      },
     },
-    plugins: [],
+  },
+  plugins: [],
 };
